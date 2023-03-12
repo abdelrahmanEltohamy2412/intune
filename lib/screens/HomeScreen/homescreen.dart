7ccc515b9d27aa902b79bef9740bc594816dc18a
reem <https://github.com/reem175>
2023-03-13 01:47:17 +0200

    edit by reem

--- lib/screens/HomeScreen/homescreen.dart
+++ lib/screens/HomeScreen/homescreen.dart
@@ -1,12 +1,11 @@
-
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:intune/const/Navigetor/Navigetor.dart';
 import 'package:intune/const/colors/colors.dart';
 import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 import '../BottomNavigationBar/Home.dart';
 import '../BottomNavigationBar/chat.dart';
 import '../QuizScreens/patient_Photo_Screen.dart';
-
 
 class BottomBarScreen extends StatefulWidget {
   const BottomBarScreen({super.key});
@@ -51,68 +50,58 @@ class _BottomBarScreenState extends State<BottomBarScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: pages[_selectedPageIndex]['page'],
-      bottomNavigationBar: BottomAppBar(
-        // color: Colors.white,
-        shape: const CircularNotchedRectangle(),
-        notchMargin: -15,
-        clipBehavior: Clip.antiAlias,
-        child: SizedBox(
-          height: 54.5,
+      bottomNavigationBar: Container(
+        decoration: const BoxDecoration(
+          borderRadius: BorderRadius.only(
+              topRight: Radius.circular(30), topLeft: Radius.circular(30)),
+          boxShadow: [
+            BoxShadow(color: Colors.black38, spreadRadius: 0, blurRadius: 10),
+          ],
+        ),
+        child: BottomAppBar(
+          shape: const CircularNotchedRectangle(),
+          notchMargin: -2,
+          clipBehavior: Clip.antiAlias,
           child: Container(
-            decoration: const BoxDecoration(
-              color: Colors.white,
-
-            ),
-            child: BottomNavigationBar(
-              onTap: _selectPage,
-              backgroundColor: Theme.of(context).primaryColor,
-              unselectedItemColor:MyColor.lightGrey,
-              selectedItemColor: MyColor.lightPink,
-              currentIndex: _selectedPageIndex,
-              items: const [
-                BottomNavigationBarItem(
-                    icon: FaIcon(FontAwesomeIcons.house),
-                    label: ''
-                ),
-                BottomNavigationBarItem(
-                    icon: Icon(
-                      MdiIcons.chat
-                    ),
-                    label: ''
-                ),
-                BottomNavigationBarItem(
-                    activeIcon: null,
-                    icon: Icon(null),
-                    label: ''
-                ),
-                BottomNavigationBarItem(
-                  icon:FaIcon(FontAwesomeIcons.stethoscope),
-                  label: '',
-                ),
-                BottomNavigationBarItem(
-                    icon: Icon(Icons.person_outline),
-                    label: ''
-                ),
-              ],
+            decoration: BoxDecoration(borderRadius: BorderRadius.circular(30)),
+            child: SizedBox(
+              height: 54.5,
+              child: BottomNavigationBar(
+                onTap: _selectPage,
+                backgroundColor: Theme.of(context).primaryColor,
+                unselectedItemColor: MyColor.lightGrey,
+                selectedItemColor: MyColor.lightPink,
+                currentIndex: _selectedPageIndex,
+                items: const [
+                  BottomNavigationBarItem(
+                      icon: FaIcon(FontAwesomeIcons.house), label: ''),
+                  BottomNavigationBarItem(icon: Icon(MdiIcons.chat), label: ''),
+                  BottomNavigationBarItem(
+                      activeIcon: null, icon: Icon(null), label: ''),
+                  BottomNavigationBarItem(
+                    icon: FaIcon(FontAwesomeIcons.stethoscope),
+                    label: '',
+                  ),
+                  BottomNavigationBarItem(
+                      icon: Icon(Icons.person_outline), label: ''),
+                ],
+              ),
             ),
           ),
         ),
       ),
       floatingActionButtonLocation:
-      FloatingActionButtonLocation.miniCenterDocked,
+          FloatingActionButtonLocation.miniCenterDocked,
       floatingActionButton: Padding(
         padding: const EdgeInsets.all(8.0),
         child: FloatingActionButton(
-          backgroundColor: MyColor.pink,
-          hoverElevation: 10,
-          splashColor: Colors.grey,
-          tooltip: 'Search',
-          elevation: 4,
-          child: const Icon(Icons.add),
-          onPressed: () => setState(() {
-            _selectedPageIndex = 2;
-          }),
-        ),
+            backgroundColor: MyColor.pink,
+            hoverElevation: 10,
+            splashColor: Colors.grey,
+            tooltip: 'Search',
+            elevation: 4,
+            child: const Icon(Icons.add),
+            onPressed: () => navigateTo(context, PatientPhotoScreen())),
       ),
     );
   }
--- lib/screens/onboarding/onboardingScreen.dart
+++ lib/screens/onboarding/onboardingScreen.dart
@@ -13,43 +13,45 @@ class OnBoardingScreen extends StatefulWidget {
   State<OnBoardingScreen> createState() => _OnBoardingScreenState();
 }
 
-class _OnBoardingScreenState extends State<OnBoardingScreen>  with TickerProviderStateMixin {
-
+class _OnBoardingScreenState extends State<OnBoardingScreen>
+    with TickerProviderStateMixin {
   @override
   Widget build(BuildContext context) {
-    var screens = IntroScreens(skipText: '',containerBg: MyColor.lightGreen,
-        onDone: () =>
-            Navigator.push(context, MaterialPageRoute(builder: (_)=>const SignScreen())),
+    var screens = IntroScreens(
+        skipText: '',
+        containerBg: MyColor.lightGreen,
+        onDone: () => Navigator.push(
+            context, MaterialPageRoute(builder: (_) => const SignScreen())),
         inactiveDotColor: Colors.white,
-        footerBgColor:Colors.white,
-
+        footerBgColor: Colors.white,
         activeDotColor: Colors.black,
-        footerRadius: 18.0,textColor: Colors.black,
+        footerRadius: 18.0,
+        textColor: Colors.black,
         slides: [
-          IntroScreen(headerBgColor: MyColor.lightGreen,
+          IntroScreen(
+            headerBgColor: MyColor.lightGreen,
             title: 'Welcome to intune ',
             description:
-            'intune is smart stethoscope combined with 3ECG bands and temperature sensor',
-            imageAsset: 'lib/assets/images/Group 60.png',
+                'intune is smart stethoscope combined with 3ECG bands and temperature sensor',
+            imageAsset:
+                'lib/assets/images/—Pngtree—smart medical illustration of sick_6574169.png',
           ),
-          IntroScreen(headerBgColor: MyColor.lightGreen,
+          IntroScreen(
+            headerBgColor: MyColor.lightGreen,
             title: 'The diagnosis become easier',
             description:
-            'intune help you to detect the heart and respiratory system Diseases in easy way',
+                'intune help you to detect the heart and respiratory system Diseases in easy way',
             imageAsset: 'lib/assets/images/Group 61.png',
           ),
-          IntroScreen(headerBgColor: MyColor.lightGreen,
+          IntroScreen(
+            headerBgColor: MyColor.lightGreen,
             title: 'Keep your medical information ',
             description:
-            'Intune enable you to make a medical history to save the results of diagnosis'
-            ,
-            imageAsset: 'lib/assets/images/Group 62.png',
+                'Intune enable you to make a medical history to save the results of diagnosis',
+            imageAsset: "",
           )
         ]);
 
-
-    return Scaffold(body:screens
-
-    );
+    return Scaffold(body: screens);
   }
 }
